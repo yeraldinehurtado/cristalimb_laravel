bd920544ffb255a487aa2451761857f77b8bdb64
Yeraldine Hurtado Muñoz <yeraldinehurtado1@gmail.com>
2021-09-25 22:58:40 -0500

    .

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -22,7 +22,7 @@
             <div class="container">
                 <a class="navbar-brand" href="#page-top"><img src="{{ asset('assets/landing/assets/img/logocristal.jpg')}}" width="110" height="100" class="rounded" alt="..." /></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
+                    Menú
                     <i class="fas fa-bars ms-1"></i>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
