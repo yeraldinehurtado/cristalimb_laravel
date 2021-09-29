268f6b4f768eb2cef6a97308efa1e8cc6370aef2
Yeraldine Hurtado Muñoz <yeraldinehurtado1@gmail.com>
2021-09-29 02:28:59 -0500

    Pequeño cambio en quiénes somos

--- resources/views/landing/quienessomos.blade.php
+++ resources/views/landing/quienessomos.blade.php
@@ -3,9 +3,6 @@
 
 <header class="masthead">
             <div class="container">
-                <div class="masthead-subheading">Welcome To Our Studio!</div>
-                <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
-                <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
             </div>
         </header>
         <!-- Services-->
